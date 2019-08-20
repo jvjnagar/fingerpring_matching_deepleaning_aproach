44fdb6fc2b410d86318cb3d52735862280d8b062
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2019-08-20 12:42:51 +0530

    resnet50 training network

---
+++ vjresnet50.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 13:08:23 2019
+
+@author: IITI204
+"""
+
+#class person():
+#    age:int
+#    name:str
+#    is_act: bool= True
+#    
+    
+
+from __future__ import print_function
+import keras
+import numpy as np
+import warnings
+
+from keras.layers import Input
+from keras import layers
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import GlobalMaxPooling2D
+from keras.layers import ZeroPadding2D
+from keras.layers import AveragePooling2D
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import BatchNormalization
+from keras.models import Model
+from keras.preprocessing import image
+import keras.backend as K
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import preprocess_input
+from keras_applications.imagenet_utils import _obtain_input_shape
+from keras.engine.topology import get_source_inputs
